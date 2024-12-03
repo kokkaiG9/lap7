535c95ff33d314f4fd246102c63b6d0a25b40af0
kokkaiG9 <gowsky.9.2548@gmail.com>
2024-12-03 16:34:06 +0700

    finish

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x == 'A'){
+		return 'Z';
+	}else if('A'< x && x <= 'Z'){
+		return x-1;
+	}else{
+		return '0';
+	}
 }
 
 int main(){
